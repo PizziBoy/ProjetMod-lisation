1c2567e7a21600ab37ca991f38f0c7b5e25944ea
thomasca81 <thomca81@gmail.com>
2020-11-18 18:04:43 +0100

    Refactoring

--- ItemBased.py
+++ ItemBased.py
@@ -19,20 +19,30 @@ from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import numpy as np
 import math
+from sklearn.impute import KNNImputer
+
+
+#------------------------------------------------------------------------------------------------------------------------
+
+#                           Fonctions outils
+
+#-----------------------------------------------------------------------------------------------------------------------
 
 
 
+# Retourne la somme des élements d'une liste passée en paramètre
 def sommeListe(liste):
     _somme = 0
     for i in liste:
         _somme = _somme + int(i)
     return _somme
 
+# Retourne la moyenne des élements d'une liste passée en paramètre
+def average(liste):
+    assert len(liste) > 0
+    return float(sommeListe(liste)) / len(liste)
 
-def average(x):
-    assert len(x) > 0
-    return float(sommeListe(x)) / len(x)
-
+# Retourne la similarité 
 def pearson(x, y):
     assert len(x) == len(y)
     n = len(x)
@@ -51,15 +61,25 @@ def pearson(x, y):
 
     return diffprod / math.sqrt(xdiff2 * ydiff2)
 
-
+# Retourne la similarité
 def cos_sim(a, b):
         dot_product = np.dot(a, b)
         norm_a = np.linalg.norm(a)
         norm_b = np.linalg.norm(b)
         return dot_product / (norm_a * norm_b)
 
-#Retourne une liste des 10 meilleures similarités par rapport a un item donné
-def simList(dataSet, item, nItems):
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+#                               Fonctions pour la prédiction
+
+#----------------------------------------------------------------------------------------------------------------------- 
+ 
+ 
+      
+# Retourne la liste des 10 meilleures similarités par rapport a un item donné
+def simList(dataSet, item):
     simListByUser = {}
     #simListUser = {}
     for i in range(0, 1000):
@@ -71,7 +91,7 @@ def simList(dataSet, item, nItems):
     return dict(reversed(sorted(simListByUser.items(), key=lambda item: item[1])))
 
 
-
+# Cette fonction retourne une liste comprenant les deux listes passées en paramètre avec les valeurs -1 supprimées
 def etLogique(u0, u1):
     liste0 = []
     liste1 = []
@@ -84,10 +104,15 @@ def etLogique(u0, u1):
     les2Listes.append(liste1)
     return les2Listes
 
+def removeDonnéeManquante(item):
+    list = []
+    for i in range(item.size):
+        if not(i == -1):
+            list.append(i)
+    return list
 
 
-
-#Renvoie les indices des données manquantes d'un item
+#Renvoie les indices des données manquantes d'un item passé en paramètre
 def rechercheDonnéeManquante(item):
     list = []
     s = 0
@@ -98,35 +123,46 @@ def rechercheDonnéeManquante(item):
     return list
 
 #Renvoie une liste de toutes les notes item d'un même utilisateur
-def noteList(simList, user):
+def noteUserList(simListItem, user):
     list = []
     for i in simList.keys():
         list.append(user[i])
     return list
 
-#Calcule une note en fonction de les similarités et les notes (10 pour 10 premiers utilisateurs)
-def calculNote(simList, noteList):
+#Calcule une note en fonction des similarités 
+def calculNote(simList, noteUserList):
         resultPoids = 0
         sommeSim = 0
-        simList = etLogique(simList, noteList)[0]
-        noteList = etLogique(simList, noteList)[1]
+        simList = etLogique(simList, noteUserList)[0]
+        noteUserList = etLogique(simList, noteUserList)[1]
         i = 0
         for i in range(0, 4):
-            resultPoids += simList[i] * noteList[i]
+            resultPoids += simList[i] * noteUserList[i]
             sommeSim += simList[i]
         return resultPoids/sommeSim
 
 
-#Prédis les valeurs manquantes d'un utilisateur
+
+#-----------------------------------------------------------------------------------------------------------------
+        
+#                                   Prediction de notes
+        
+#-----------------------------------------------------------------------------------------------------------------
+
+#J'ouvre les dataSet toy_incomplet et toy_complet
+dataSetJouet = pd.read_csv("toy_incomplet.csv", sep=' ', header = None)
+dataSetJouetComplet = pd.read_csv("toy_incomplet.csv", sep=' ', header = None)
+
+#Prédis les valeurs manquantes d'un item
 def predictItem(item, dataSetJouet):
     listeNotesManquantes = rechercheDonnéeManquante(item)
-    listeSimilarite = simList(dataSetJouet, item, 10)
+    listeSimilarite = simList(dataSetJouet, item)
     for i in listeNotesManquantes:
-        listeNotesCourantes = noteList(listeSimilarite, dataSetJouet.loc[i])
-        item[i] = calculNote(listeSimilarite, listeNotesCourantes)
+        listeNotesCourantes = noteUserList(listeSimilarite, dataSetJouet.loc[i])
+        item[i] = calculNote(listeSimilarite, listeNotesCourantes, item[i])
     return item
         
-
+# Prédis les valeurs manquantes pour tout le dataSet
 def predictAll(dataSetJouet):
     newDf = pd.DataFrame(lines = range(100))
     for i in range(0, 999):
@@ -134,22 +170,22 @@ def predictAll(dataSetJouet):
         newDf = pd.concat([newDf, tmp.to_frame().T], ignore_index=True)
     return newDf
 
+# Retourne la différence entre nos predictions et les veritables notes 
 def diff(dataSet1, dataSet2):
     newDf = pd.DataFrame()
     for i in range(100):
         newDf[i] = np.where(dataSet1[i] == dataSet2[i], 0, dataSet1[i] - dataSet2[i])
     return newDf
 
-#J'ouvre le dataSet toy_incomplet
-dataSetJouet = pd.read_csv("toy_incomplet.csv", sep=' ', header = None)
-dataSetJouetComplet = pd.read_csv("toy_incomplet.csv", sep=' ', header = None)
-#.loc[0] = ligne 0 et [0] = colonne 0
-#print(calculNote([0.72, 0.55, 0.33], [2, 3, 5]))
-#print(simList(dataSetJouet, dataSetJouet[0], 10))
+
+# Je calcule la prédiction de notes pour l'Item 1
 print(predictItem(dataSetJouet[0], dataSetJouet))
 
-from sklearn.impute import KNNImputer
+# Je calcule l'écart de mes predictions avec la valeurs réelles
+#print(diff(dataSetJouetComplet,predictItem(dataSetJouet[0], dataSetJouet)))
 
-imputer = KNNImputer(n_neighbors=5, missing_values=-1)
 
-print(diff(dataSetJouetComplet,predictItem(dataSetJouet[0], dataSetJouet)))
+# Methode des KNN
+#imputer = KNNImputer(n_neighbors=5, missing_values=-1)
+#print(imputer.fit_transform(dataSetJouet))
+#print(diff(dataSetJouetComplet,predictItem(dataSetJouet[0], dataSetJouet)))
